52b57ab33227b2be3409aa20969ccf31c90ade3f
ahadsheraz96 <40655106+ahadsheraz96@users.noreply.github.com>
2019-12-16 15:55:37 -0500

    Add files via upload

---
+++ Call2.java
@@ -0,0 +1,8 @@
+public class Call2 {
+
+
+    public static void exampleNumber(){
+        System.out.println("my number is 516666666");
+    }
+
+}
---
+++ Calling.java
@@ -0,0 +1,8 @@
+public class Calling {
+    public static void main (String arg[]){
+        System.out.println("hey we are using object from another class");
+        Call2 newobj= new Call2();
+        newobj.exampleNumber();
+        
+    }
+}
